40a528c2ed4f229e545ab53748df1a49bffdf1dc
Tyler <calhountylerk@gmail.com>
2022-12-22 17:21:41 -0800

    Fixed corner collisions

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,8 @@ async fn main() {
     let bottom = Rectangle::new(Vec2::new(1.0, screen_height() - 1.0), Vec2::new((screen_width() - 1.0) * 2.0, 1.0));
     //let origin = Rectangle::new(Vec2::new(130.0, 130.0), Vec2::new(50.0, 50.0));
 
-
-    //let mut ball = Ball::new(Vec2::new(screen_width() / 2.0, screen_height() / 2.0), Vec2::new(-1.0, -0.50));
     let initial_direction = Vec2::new(screen_width(), screen_height()).normalize();
     let mut ball = Ball::new(Vec2::new(screen_width() / 2.0, screen_height() / 2.0), initial_direction);
-    let mut can_collide = true;
 
     scene.push(&left);
     scene.push(&right);
@@ -32,38 +29,25 @@ async fn main() {
         //origin.draw(WHITE);
 
         let scene_info = distance_to_scene(&scene, Vec2::new(ball.position.x, ball.position.y));
-        let closest_distance = scene_info.0;
-        let closest_rectangle = scene_info.1;
+        match scene_info {
+            Some(rectangles) => {
+                ball.draw_ball(RED);
+                for rectangle in rectangles {
+                    let normal = calculate_collision_normal(
+                        Vec2::new(ball.position.x, ball.position.y),
+                        rectangle.size,
+                        rectangle.position
+                    );
+
+                    let collision_trajectory = calculate_collision_trajectory(ball.direction, normal);
+                    ball.direction = collision_trajectory;
+
+                }
+            },
+            None => ball.draw_ball(GREEN),
+        };
 
         ball.position = ball.position + ball.direction * get_frame_time() * 300.0;
-        //ball.direction.y = ball.direction.y + 980.0 * get_frame_time();
-        //ball.position = Vec2::new(mouse_position_x, mouse_position_y);
-
-        if closest_distance >= 15.0 {
-             can_collide = true;
-        }
-        if closest_distance < 15.0 && can_collide {
-            can_collide = false;
-            ball.draw_ball(RED);
-            let normal = calculate_collision_normal(
-                Vec2::new(ball.position.x, ball.position.y),
-                closest_rectangle.size,
-                closest_rectangle.position
-            );
-
-            let collision_trajectory = calculate_collision_trajectory(ball.direction, normal);
-            //println!("trajectory before collision {} {}", ball.direction.x, ball.direction.y);
-            //println!("normal {} {}", normal.x, normal.y);
-            //println!("trajectory after collision {} {}", collision_trajectory.x, collision_trajectory.y);
-            println!("distance to scene: {}", closest_distance);
-            ball.direction = collision_trajectory;
-
-            ball.position.x = ball.position.x + 2.0 * ball.direction.x;
-            ball.position.y = ball.position.y + 2.0 * ball.direction.y;
-
-        } else {
-            ball.draw_ball(GREEN);
-        }
 
         next_frame().await
     }
@@ -77,9 +61,7 @@ fn sdf2(point: Vec2, size: Vec2, position: Vec2) -> f32 {
     let d = Vec2::length(Vec2::max(q, Vec2::ZERO) + f32::min(q.max_element(), 0.0));
     d
 }
-// p, the position of the collision point
-// r, [rectangle width, rectangle height]
-// position, the position of the rectangle
+
 fn calculate_collision_normal(p: Vec2, r: Vec2, position: Vec2) -> Vec2 {
     let p = p - position;
     
@@ -94,21 +76,24 @@ fn calculate_collision_trajectory(direction: Vec2, normal: Vec2) -> Vec2 {
     direction.normalize()
 }
 
-fn distance_to_scene(scene: &Vec<&Rectangle>, point: Vec2) -> (f32, Rectangle) {
-    let mut distance = f32::INFINITY;
-    let mut closest_rectangle = scene[0];
+fn distance_to_scene(scene: &Vec<&Rectangle>, point: Vec2) -> Option<Vec<Rectangle>> {
+    let mut rectangles: Vec<Rectangle> = Vec::new();
+
     for rectangle in scene {
-        //let distance_to_rectangle = sdf(point, rectangle.size, rectangle.position);
         let distance_to_rectangle = sdf2(point, rectangle.size, rectangle.position);
-        if distance_to_rectangle < distance {
-            distance = distance_to_rectangle;
-            closest_rectangle = &rectangle;
+        let rec = rectangle.clone();
+        if distance_to_rectangle < 15.0 {
+            rectangles.push(*rec);
         }
     }
-
-    (distance, Rectangle::new(closest_rectangle.position, closest_rectangle.size))
+    if rectangles.len() > 0 {
+        Some(rectangles)
+    } else {
+        None
+    }
 }
 
+#[derive(Copy, Clone)]
 struct Rectangle {
     position: Vec2,
     size: Vec2,
